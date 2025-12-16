be1011c546b9b0e0278ae7502b5e54bdc9a47484
Charishma-2000 <Charishma.E@ibm.com>
2025-12-16 21:15:55 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity14.java
@@ -0,0 +1,31 @@
+package Example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import java.util.List;
+
+public class Activity14 {
+	
+	public static void main(String[] args) {
+		  
+		   WebDriver driver = new FirefoxDriver();
+				    
+		   driver.get("https://training-support.net/webelements/tables");
+				   
+		   System.out.println("Title is: "+driver.getTitle());
+		   
+		   WebElement row5 = driver.findElement(By.xpath("//table/tbody/tr[5]/td[2]"));
+	       System.out.println("Book Name in the 5th row is: " + row5.getText());
+	       
+	       driver.findElement(By.xpath("//th[text()='Price']")).click();
+	       
+	       System.out.println("Clicked on header of the Price column");
+	       
+	       row5 = driver.findElement(By.xpath("//table/tbody/tr[5]/td[2]"));
+	       System.out.println("Book Name in the 5th row is: " + row5.getText());
+	       
+	       driver.quit();
+	}
+}
---
+++ Selenium/Activities/activity14.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+with webdriver.Firefox() as driver:
+   
+   driver.get("https://training-support.net/webelements/tables")
+   
+   print("Page title is: ", driver.title)
+    
+   row5 = driver.find_element(By.XPATH, "//table/tbody/tr[5]/td[2]")
+   print("Book Name in the 5th row is: ", row5.text)
+
+   driver.find_element(By.XPATH, "//th[text()='Price']").click()
+
+   print("Clicked on header of the Price column")
+
+   row5 = driver.find_element(By.XPATH, "//table/tbody/tr[5]/td[2]")
+   print("Book Name in the 5th row is: ", row5.text)
+
+driver.quit()
